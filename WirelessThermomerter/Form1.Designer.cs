549323aa0e49d66b14a51f995bce529695cdc5eb
sgtsuzuki <msgsuzuki@gmail.com>
2025-07-06 20:28:33 +0900

    プロジェクト ファイルを追加します。

---
+++ WirelessThermomerter/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WirelessThermomerter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.celsiusLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "接続";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(313, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "カウント＝";
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.countLabel.Location = new System.Drawing.Point(399, 12);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(19, 19);
+            this.countLabel.TabIndex = 3;
+            this.countLabel.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(436, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "温度＝";
+            // 
+            // celsiusLabel
+            // 
+            this.celsiusLabel.AutoSize = true;
+            this.celsiusLabel.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.celsiusLabel.Location = new System.Drawing.Point(508, 13);
+            this.celsiusLabel.Name = "celsiusLabel";
+            this.celsiusLabel.Size = new System.Drawing.Size(33, 19);
+            this.celsiusLabel.TabIndex = 5;
+            this.celsiusLabel.Text = "0.0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(557, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 19);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "℃";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 57600;
+            this.serialPort1.ErrorReceived += new System.IO.Ports.SerialErrorReceivedEventHandler(this.serialPort1_ErrorReceived);
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(603, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "CSV出力";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(221, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "切断";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(15, 42);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(663, 438);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 492);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.celsiusLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.Name = "Form1";
+            this.Text = "ワイヤレス温度測定アプリ";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label celsiusLabel;
+        private System.Windows.Forms.Label label5;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ WirelessThermomerter/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO.Ports; // COMポート通信のための名前空間
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WirelessThermomerter
+{
+    public partial class Form1 : Form
+    {
+
+        // 取得データの履歴
+        const int MAX_HISTORY = 100;
+        Queue<double> tempHistory = new Queue<double>();
+
+        string id;
+        string temperature;
+
+        List<string> idList = new List<string>();
+        List<string> tempList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // チャートの表示を初期化
+            initChart(chart1);
+
+            // 1秒周期でチャートを再描画
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+
+            // 起動時にCOMポートの一覧リストを取得してコンボボックスに設定
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+            if (ports.Length > 0)
+            {
+                comboBox1.SelectedIndex = 0; // 最初のポートを選択
+            }
+        }
+
+        private void initChart(Chart chart)
+        {
+
+            // チャート全体の背景色を設定
+            chart.BackColor = Color.Black;
+            chart.ChartAreas[0].BackColor = Color.Transparent;
+
+            // チャート表示エリア周囲の余白をカットする
+            chart.ChartAreas[0].InnerPlotPosition.Auto = false;
+            chart.ChartAreas[0].InnerPlotPosition.Width = 100; // 100%
+            chart.ChartAreas[0].InnerPlotPosition.Height = 90;  // 90%(横軸のメモリラベル印字分の余裕を設ける)
+            chart.ChartAreas[0].InnerPlotPosition.X = 8;
+            chart.ChartAreas[0].InnerPlotPosition.Y = 0;
+
+
+            // X,Y軸情報のセット関数を定義
+            Action<Axis> setAxis = (axisInfo) =>
+            {
+                // 軸のメモリラベルのフォントサイズ上限値を制限
+                axisInfo.LabelAutoFitMaxFontSize = 8;
+
+                // 軸のメモリラベルの文字色をセット
+                axisInfo.LabelStyle.ForeColor = Color.White;
+
+                // 軸タイトルの文字色をセット(今回はTitle未使用なので関係ないが...)
+                axisInfo.TitleForeColor = Color.White;
+
+                // 軸の色をセット
+                axisInfo.MajorGrid.Enabled = true;
+                axisInfo.MajorGrid.LineColor = ColorTranslator.FromHtml("#008242");
+                axisInfo.MinorGrid.Enabled = false;
+                axisInfo.MinorGrid.LineColor = ColorTranslator.FromHtml("#008242");
+            }
+            ;
+
+            // X,Y軸の表示方法を定義
+            setAxis(chart.ChartAreas[0].AxisY);
+            setAxis(chart.ChartAreas[0].AxisX);
+            chart.ChartAreas[0].AxisX.MinorGrid.Enabled = true;
+            chart.ChartAreas[0].AxisY.Maximum = 500;    // 縦軸の最大値 orignal 100
+
+            chart.AntiAliasing = AntiAliasingStyles.None;
+
+            // 折れ線グラフとして表示
+            chart.Series[0].ChartType = SeriesChartType.FastLine;
+            // 線の色を指定
+            chart.Series[0].Color = ColorTranslator.FromHtml("#00FF00");
+
+            // 凡例を非表示,各値に数値を表示しない
+            chart.Series[0].IsVisibleInLegend = false;
+            chart.Series[0].IsValueShownAsLabel = false;
+
+            // チャートに表示させる値の履歴を全て0クリア
+            while (tempHistory.Count <= MAX_HISTORY)
+            {
+                tempHistory.Enqueue(0);
+            }
+        }
+
+
+
+        private void showChart(Chart chart)
+        {
+            //-----------------------
+            // チャートに値をセット
+            //-----------------------
+            chart.Series[0].Points.Clear();
+            foreach (int value in tempHistory)
+            {
+                // データをチャートに追加
+                chart.Series[0].Points.Add(new DataPoint(0, value));
+            }
+        }
+
+        /// <summary>
+        /// COMポートの接続ボタンがクリックされたときの処理 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 既にCOMポートは開いているか？
+            if (serialPort1.IsOpen)
+            {
+                // MessageBox.Show("COMポートは既に開いています。");
+                return; // 既に開いている場合は何もしない
+            }
+
+            if (comboBox1.SelectedItem != null)
+            {
+                string selectedPortNumber = comboBox1.SelectedItem.ToString();
+                try
+                {
+                    serialPort1.PortName = selectedPortNumber; // 選択されたCOMポートを設定
+                    serialPort1.Open();
+                    serialPort1.DiscardInBuffer(); // 受信バッファをクリア
+                    idList.Clear(); // IDリストをクリア
+                    tempList.Clear(); // 温度リストをクリア
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("COMポートの接続に失敗しました: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("COMポートを選択してください。");
+            }
+
+        }
+
+        // COMポート受信時の処理
+        // "475,28.75\r\n"
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                // データ受信処理
+                //using (var serialPort1 = (SerialPort)sender)
+                //{
+                string data = serialPort1.ReadLine();
+                Console.WriteLine("受信データ: " + data);
+                //string data = serialPort1.ReadExisting();
+
+                // 受信データをチェック 1,25.5 の様な形式なのでカンマ区切りで分離
+                string[] parts = data.Split(',');
+                if (parts.Length == 2)
+                {
+                    id = parts[0].Trim(); // ID部分
+                    temperature = parts[1].Trim(); // 温度部分
+
+                }
+                else
+                {
+                    Console.WriteLine("受信データの形式が不正です: " + data);
+                    return; // 不正なデータは無視
+                }
+
+                idList.Add(id); // IDをリストに追加
+                tempList.Add(temperature);  // 温度をリストに追加
+
+                this.Invoke(new MethodInvoker(() => countLabel.Text = id)); // IDをラベルに表示
+                this.Invoke(new MethodInvoker(() => celsiusLabel.Text = temperature)); // 温度をラベルに表示
+
+                // setChartData(temperature);
+                this.Invoke(new MethodInvoker(() => setChartData(temperature)));
+                //}
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("データ受信中にエラーが発生しました: " + ex.Message);
+            }
+        }
+
+        private void serialPort1_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            Console.WriteLine("serialPort1_ErrorReceived");
+            Console.WriteLine(e.EventType.ToString());
+        }
+
+        /// <summary>
+        /// COMポートの切断ボタンがクリックされたときの処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // シリアルを切断
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Close();
+                    MessageBox.Show("COMポートを切断しました。");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("COMポートの切断に失敗しました: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("COMポートは開いていません。");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // idListとtempListの内容をCSVに出力する ファイル名は固定で良い
+            string filePath = "output.csv"; // 出力ファイル名
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePath))
+                {
+                    // ヘッダーを書き込む
+                    file.WriteLine("ID,Temperature");
+                    // 各IDと温度をCSV形式で書き込む
+                    for (int i = 0; i < idList.Count; i++)
+                    {
+                        file.WriteLine($"{idList[i]},{tempList[i]}");
+                    }
+                }
+                MessageBox.Show("データをCSVファイルに出力しました: " + filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("CSVファイルの出力に失敗しました: " + ex.Message);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ////---------------------------------
+            //// CPUの使用率を取得し、履歴に登録
+            ////---------------------------------
+            //// int value = (int)pc.NextValue();
+            //var temp = 0;
+
+            //tempHistory.Enqueue(temp);
+
+            ////------------------------------------------------
+            //// 履歴の最大数を超えていたら、古いものを削除する
+            ////------------------------------------------------
+            //while (tempHistory.Count > MAX_HISTORY)
+            //{
+            //    tempHistory.Dequeue();
+            //}
+
+            ////------------------------------------------------
+            //// グラフを再描画する
+            ////------------------------------------------------
+            //showChart(chart1);
+        }
+
+
+        private void setChartData(string temptureString)
+        {
+            // temptureString を double値に変換
+            double temp = double.Parse(temptureString);
+            tempHistory.Enqueue(temp);
+
+            //------------------------------------------------
+            // 履歴の最大数を超えていたら、古いものを削除する
+            //------------------------------------------------
+            while (tempHistory.Count > MAX_HISTORY)
+            {
+                tempHistory.Dequeue();
+            }
+
+            //------------------------------------------------
+            // グラフを再描画する
+            //------------------------------------------------
+            showChart(chart1);
+
+        }
+    }
+
+}
